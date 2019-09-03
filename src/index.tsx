6ecfef1e293030ee65d424a653d634837cd879e7
Shyam Patel <spatel@bastiansolutions.com>
2019-09-03 11:47:09 -0400

    Added page routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import About from './About';
+import PageNotFound from './PageNotFound';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <Switch>
+            <Route exact path="/" component={App}></Route>
+            <Route path="/about" component={About}></Route>
+            <Route component={PageNotFound}></Route>
+            </Switch>
+        </div>
+    </Router>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(routing, document.getElementById('root'));
