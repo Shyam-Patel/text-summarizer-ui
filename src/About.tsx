7c44bd14318e078e5afa91c4d94e69c1f5747fc1
Shyam Patel <spatel@bastiansolutions.com>
2019-09-04 11:54:51 -0400

    Added jQuery & PrimeReact dependences + tested API calling

--- src/About.tsx
+++ src/About.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const About: React.FC = () => {
-    return (<h1>Shyam Patel</h1>)
+    return (
+            <h1>Shyam Patel</h1>
+    );
 }
 
 export default About
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,59 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to iSummarize!
-        </p>
-      </header>
-    </div>
-  );
+
+import {Button} from 'primereact/button';
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+
+type catFactModel = {
+  _id: Object,
+  _v: Number,
+  user: Object,
+  text: string,
+  updatedAt: Object,
+  sendDate: Object,
+  deleted: boolean,
+  source: string,
+  used: boolean,
+  type: string
+}
+
+
+export class App extends React.Component {
+  
+  handleClick(): void{
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    fetch(proxyurl + "https://cat-fact.herokuapp.com/facts/random")
+      .then(response => response.json())
+      .then(
+        (result) => {
+          var factModel: catFactModel = result as catFactModel;
+          $("#apiResult").text(factModel.text);
+        },
+        (error) => {
+          $("#apiResult").text("Error contacting API: " + error);
+        }
+      )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome to iSummarize!
+          </p>
+          <Button label="Call cats API" className="p-button-raised" onClick={this.handleClick}/>
+          <p id="apiResult"></p>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
